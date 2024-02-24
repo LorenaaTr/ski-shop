744aebe2e342e4aa1e477c83b5184d1d0bfef183
LorenaaTr <lorenatroshupa@gmail.com>
2024-02-24 17:44:22 +0100

    Product

--- API/Program.cs
+++ API/Program.cs
@@ -31,4 +31,18 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+var scope = app.Services.CreateScope();
+var context = scope.ServiceProvider.GetRequiredService<StoreContext>();
+var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+try
+{
+    context.Database.Migrate();
+    DbInitializer.Initialize(context);
+}
+catch (Exception ex)
+{
+    
+    logger.LogError(ex, "An error occured during migration!");
+}
+
 app.Run();
