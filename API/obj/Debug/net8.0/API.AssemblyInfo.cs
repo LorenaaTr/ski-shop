709af4a737c071729b33a20f6fc92769cf245610
LorenaaTr <lorenatroshupa@gmail.com>
2024-03-08 11:38:02 +0100

    NavBar

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6eb08afaf0e266e4650722190510a2ddb63ec1c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6c8f8f4595baf377bd735f0d199be4cdf5edd29")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ client/src/features/catalog/ProductDetails.tsx
@@ -0,0 +1,12 @@
+import { Typography } from "@mui/material";
+import { useParams } from "react-router-dom";
+
+export default function ProductDetails(){
+    return(
+        <>
+        <Typography variant="h2">
+            ProductDetails  
+        </Typography>
+        </>
+    )
+}
