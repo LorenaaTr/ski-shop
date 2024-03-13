75e7c403ecdf5ef5dfe40d66e87c5910853b7dd7
LorenaaTr <lorenatroshupa@gmail.com>
2024-03-13 23:50:49 +0100

    errors handeling and loading page

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5e64e96f06639e520f1f033c6ce43e8ed0fc3a9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4e7af2d0872d5ad78eafd7917388f25ad3c5562")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
