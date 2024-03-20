d8c17ac63062bf46909062d4e28d24dc207847f3
LorenaaTr <lorenatroshupa@gmail.com>
2024-03-20 22:25:57 +0100

    Checkout Page

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ac3bdeb185ec3aa19934c0f8dd3e77f90b5589d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c4a06bd43b931d1ca15b1f1eacbd739c4fe910f")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
